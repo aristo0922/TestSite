7782596a574005eb5a2af28d6ebbae1da80a44bd
aristo <dkfud2121@naver.com>
2023-01-04 01:57:19 +0900

    fea: complete creating posting web page

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,9 +1,15 @@
+from django.contrib import admin
 from django.urls import path
 from .views import *
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', index),
-    path('blog/', blog),
+    path('admin/', admin.site.urls),
+
+    path('', index, name='index'),
+    path('blog/', blog, name='blog'),
+    path('blog/new_post/', new_post),
+    path('blog/<int:pk>', posting, name="posting"),
+    path("blog/<int:pk>/remove/", remove_post),
 ]
